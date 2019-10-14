696b0f5ff17be660c577ec208809f89b41d587e9
at-vinh.huynh <vinh.huynh@asiantech.vn>
2019-10-14 11:08:49 +0700

    add firebase appId

--- Deploy.Jenkinsfile
+++ Deploy.Jenkinsfile
@@ -49,7 +49,7 @@ pipeline {
 
                 writeFile file: "release_notes.txt", text: "${BRANCH_NAME}: #${BUILD_ID}\n"
                 sh 'git log --oneline -1 >> release_notes.txt'
-                sh "./gradlew assemble${DEPLOY_ENV} appDistributionUpload${DEPLOY_ENV}"
+                sh "./gradlew assemble${DEPLOY_ENV} appDistributionUpload${DEPLOY_ENV} --stacktrace"
 
                 sh "rsync -au ${GRADLE_USER_HOME}/caches ${GRADLE_USER_HOME}/wrapper ${GRADLE_TEMP}/ || true"
             }
--- app/src/main/java/jenkin/android/asiantech/myapplication/MainActivity.kt
+++ app/src/main/java/jenkin/android/asiantech/myapplication/MainActivity.kt
@@ -2,8 +2,9 @@ package jenkin.android.asiantech.myapplication
 
 import android.content.ActivityNotFoundException
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 
@@ -21,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
+            val result = 1/0
+            Log.d("VVVV","${result}")
         }
 
         try {
